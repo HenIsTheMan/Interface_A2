7de5c574c6fe806f9a0c4e0798102148415e4c15
Henry <193541T@mymail.nyp.edu.sg>
2021-01-28 00:30:50 +0800

    EllipsesControl.cs

---
+++ Assets/Scripts/EllipsesControl.cs
@@ -0,0 +1,56 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace InterfaceA2 {
+    internal sealed class EllipsesControl: MonoBehaviour {
+        #region Fields
+
+        private float BT;
+        private float elapsedTime;
+        [SerializeField] private float delay;
+        [SerializeField] private TextMeshProUGUI tmpComponent;
+        private int dotCount;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        public EllipsesControl() {
+            BT = 0.0f;
+            elapsedTime = 0.0f;
+            delay = 0.0f;
+            tmpComponent = null;
+            dotCount = 0;
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void Awake() {
+            UnityEngine.Assertions.Assert.IsNotNull(tmpComponent);
+        }
+
+        private void Update() {
+            elapsedTime += Time.deltaTime;
+
+            if(BT <= elapsedTime) {
+                while(tmpComponent.text.Substring(tmpComponent.text.Length - 1) == ".") {
+                    tmpComponent.text = tmpComponent.text.Substring(0, tmpComponent.text.Length - 1);
+                }
+
+                dotCount = dotCount == 3 ? 0 : dotCount + 1;
+                for(int i = 0; i < dotCount; ++i) {
+                    tmpComponent.text += '.';
+                }
+
+                BT = elapsedTime + delay;
+            }
+        }
+
+        #endregion
+    }
+}
