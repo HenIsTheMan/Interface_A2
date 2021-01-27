0713f2d80bd67910ceb74dbf4fca0373c16943f3
Henry <193541T@mymail.nyp.edu.sg>
2021-01-27 10:57:40 +0800

    CustomizeTextMeshPro.cs

---
+++ Assets/Scripts/CustomizeTextMeshPro.cs
@@ -0,0 +1,38 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace InterfaceA2{
+    internal sealed class CustomizeTextMeshPro: MonoBehaviour {
+        #region Fields
+
+        [SerializeField] private TextMeshProUGUI tmpComponent;
+        [SerializeField] private string text;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        public CustomizeTextMeshPro() {
+            tmpComponent = null;
+            text = null;
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void Awake() {
+            UnityEngine.Assertions.Assert.IsNotNull(tmpComponent);
+            UnityEngine.Assertions.Assert.IsNotNull(text);
+        }
+
+        private void Start() {
+            tmpComponent.text = text;
+        }
+
+        #endregion
+    }
+}
