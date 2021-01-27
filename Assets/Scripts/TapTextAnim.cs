5d241e97cee547b4c9bf08a1ba2c8c0d48f8b5a6
Henry <193541T@mymail.nyp.edu.sg>
2021-01-27 19:33:40 +0800

    Removed LangDropdown.cs

---
+++ Assets/Scripts/TapTextAnim.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace InterfaceA2 {
+    internal sealed class TapTextAnim: MonoBehaviour {
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        public TapTextAnim() {
+
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+        #endregion
+    }
+}
