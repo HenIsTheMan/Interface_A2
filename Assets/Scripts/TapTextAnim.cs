131445f71c2e3544d11c8f77faf963296b889969
Henry <193541T@mymail.nyp.edu.sg>
2021-01-27 19:51:20 +0800

    Text flashing

--- Assets/Scripts/TapTextAnim.cs
+++ Assets/Scripts/TapTextAnim.cs
@@ -1,8 +1,15 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 namespace InterfaceA2 {
     internal sealed class TapTextAnim: MonoBehaviour {
         #region Fields
+
+        private float elapsedTime;
+        [SerializeField] private TextMeshProUGUI tmpComponent;
+        [SerializeField] private float startAlpha;
+        [SerializeField] private float endAlpha;
+
         #endregion
 
         #region Properties
@@ -11,12 +18,33 @@ namespace InterfaceA2 {
         #region Ctors and Dtor
 
         public TapTextAnim() {
-
+            elapsedTime = 0.0f;
+            tmpComponent = null;
+            startAlpha = 0.0f;
+            endAlpha = 0.0f;
         }
 
         #endregion
 
         #region Unity User Callback Event Funcs
+
+        private void Awake() {
+            UnityEngine.Assertions.Assert.IsNotNull(tmpComponent);
+        }
+
+        private void Update() {
+            elapsedTime += Time.deltaTime;
+        }
+
+        private void FixedUpdate() {
+           float lerpFactor = EaseInQuint(Mathf.Cos(elapsedTime * 5.0f) * 0.5f + 0.5f);
+            tmpComponent.alpha = (1.0f - lerpFactor) * startAlpha + lerpFactor * endAlpha;
+        }
+
         #endregion
+
+        private float EaseInQuint(float x) {
+            return x * x * x * x * x;
+        }
     }
 }
