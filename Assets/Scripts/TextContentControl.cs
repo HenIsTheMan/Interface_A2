6fa9b7f65adefa81d9dd78ee6592d504d95eb5a1
Henry <193541T@mymail.nyp.edu.sg>
2021-01-28 00:54:58 +0800

    TextContentControl.cs progress

---
+++ Assets/Scripts/TextContentControl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace InterfaceA2 {
+    internal sealed class TextContentControl: MonoBehaviour {
+        #region Fields
+
+        private float BT;
+        private float elapsedTime;
+        [SerializeField] private float delay;
+        [SerializeField] private string[] loadingTexts;
+        private int loadingTextsLen;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        public TextContentControl() {
+            BT = 0.0f;
+            elapsedTime = 0.0f;
+            delay = 0.0f;
+            loadingTexts = null;
+            loadingTextsLen = 0;
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void Awake() {
+            UnityEngine.Assertions.Assert.IsNotNull(loadingTexts);
+            loadingTextsLen = loadingTexts.Length;
+        }
+
+        private void Update() {
+            elapsedTime += Time.deltaTime;
+
+            if(BT <= elapsedTime) {
+                for(int i = 0; i < loadingTextsLen; ++i) {
+                }
+
+                BT = elapsedTime + delay;
+            }
+        }
+
+        #endregion
+    }
+}
