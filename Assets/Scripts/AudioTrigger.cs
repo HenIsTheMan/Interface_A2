86e34a6cb138aaa8a3055740e70f2c64d5084017
Henry <193541T@mymail.nyp.edu.sg>
2021-01-27 21:32:15 +0800

    Some audio

---
+++ Assets/Scripts/AudioTrigger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace InterfaceA2{
+    internal sealed class AudioTrigger: MonoBehaviour {
+        #region Fields
+
+        [SerializeField] private AudioSource audioSourceComponent;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        public AudioTrigger() {
+            audioSourceComponent = null;
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void Awake() {
+            UnityEngine.Assertions.Assert.IsNotNull(audioSourceComponent);
+        }
+
+        #endregion
+
+        public void OnButtonClick() {
+            audioSourceComponent.Play();
+        }
+    }
+}
