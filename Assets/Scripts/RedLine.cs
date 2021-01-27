05d61ec1e1b71a7210047b14df8015b743881f87
Henry <193541T@mymail.nyp.edu.sg>
2021-01-27 16:26:40 +0800

    Red line

---
+++ Assets/Scripts/RedLine.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace InterfaceA2{
+    internal sealed class RedLine: MonoBehaviour {
+        #region Fields
+
+        [SerializeField] private Image imgComponent;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        public RedLine() {
+            imgComponent = null;
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void Awake() {
+            UnityEngine.Assertions.Assert.IsNotNull(imgComponent);
+        }
+
+        #endregion
+
+        public void OnButtonClick() {
+            imgComponent.enabled = !imgComponent.enabled;
+        }
+    }
+}
