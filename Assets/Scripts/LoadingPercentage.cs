af6025f911d445d24bfba895b05535b178b35481
Henry <193541T@mymail.nyp.edu.sg>
2021-01-28 22:01:35 +0800

    LoadingPercentage.cs

---
+++ Assets/Scripts/LoadingPercentage.cs
@@ -0,0 +1,63 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace InterfaceA2 {
+    internal sealed class LoadingPercentage: MonoBehaviour {
+        #region Fields
+
+        private float BT;
+        private float elapsedTime;
+        [SerializeField] private float minShortDelay;
+        [SerializeField] private float maxShortDelay;
+        [SerializeField] private float minLongDelay;
+        [SerializeField] private float maxLongDelay;
+        [SerializeField] private int chanceForShortDelay;
+        private int percentage;
+        private TextMeshProUGUI tmpComponent;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        public LoadingPercentage() {
+            BT = 0.0f;
+            elapsedTime = 0.0f;
+            minShortDelay = 0.0f;
+            maxShortDelay = 0.0f;
+            minLongDelay = 0.0f;
+            maxLongDelay = 0.0f;
+            chanceForShortDelay = 0;
+            percentage = 0;
+            tmpComponent = null;
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void Awake() {
+            tmpComponent = gameObject.GetComponent<TextMeshProUGUI>();
+        }
+
+        private void Update() {
+            elapsedTime += Time.deltaTime;
+
+            if(BT <= elapsedTime) {
+                if(percentage < 100) {
+                    tmpComponent.text = (++percentage).ToString() + '%';
+                    BT = elapsedTime + (Random.Range(1, 101) <= chanceForShortDelay ? Random.Range(minShortDelay, maxShortDelay) : Random.Range(minLongDelay, maxLongDelay));
+                }
+
+                if(percentage == 100) {
+                    //??
+                    return;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
