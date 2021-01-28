66a39ba8bcbada3b8430b79d5bf79582df99bfd8
Henry <193541T@mymail.nyp.edu.sg>
2021-01-28 11:23:28 +0800

    Revert "Worse lol"
    This reverts commit 48afb22df195cf2abea32518a98eeaf1d5144ef1.

--- Assets/Scripts/Customization0.cs
+++ Assets/Scripts/Customization0.cs
@@ -28,13 +28,9 @@ namespace InterfaceA2 {
         #region Unity User Callback Event Funcs
 
         private void Start() {
-            float HCF = MathHelpers.GCD(Screen.width, Screen.height);
-            float simplestTermScreenWidth = Screen.width / HCF;
-            float simplestTermScreenHeight = Screen.height / HCF;
-
             RectTransform myRectTransform = (RectTransform)gameObject.transform;
             myRectTransform.localPosition = new Vector3(Screen.width * posMultiplierX, Screen.height * posMultiplierY, 0.0f);
-            myRectTransform.localScale = new Vector3(scaleX * Screen.width / (simplestTermScreenWidth * 200.0f), scaleY * Screen.height / (simplestTermScreenHeight * 200.0f), 1.0f);
+            myRectTransform.localScale = new Vector3(scaleX * Screen.width / 800.0f, scaleY * Screen.height / 600.0f, 1.0f);
         }
 
         #endregion
