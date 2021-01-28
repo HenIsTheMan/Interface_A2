8cb531a86deeec8e556ba3c202c1a0e7ad64e9af
Henry <193541T@mymail.nyp.edu.sg>
2021-01-28 13:47:51 +0800

    WTS

---
+++ Assets/Scripts/Customization2.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace InterfaceA2 {
+    internal sealed class Customization2: MonoBehaviour {
+        #region Fields
+
+        [SerializeField] private float posOffsetX;
+        [SerializeField] private float posOffsetY;
+        [SerializeField] private float scaleX;
+        [SerializeField] private float scaleY;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        public Customization2() {
+            posOffsetX = 0.0f;
+            posOffsetY = 0.0f;
+            scaleX = 1.0f;
+            scaleY = 1.0f;
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void Start() {
+            float aspectRatio = Screen.width / Screen.height;
+
+            RectTransform myRectTransform = (RectTransform)gameObject.transform;
+            RectTransform parentRectTransform = (RectTransform)gameObject.transform.parent;
+            myRectTransform.localPosition = new Vector3(parentRectTransform.localPosition.x + posOffsetX, parentRectTransform.localPosition.y + posOffsetY, 0.0f);
+            myRectTransform.localScale = new Vector3(
+                scaleX * aspectRatio,
+                scaleY * aspectRatio,
+                1.0f
+            );
+        }
+
+        #endregion
+    }
+}
