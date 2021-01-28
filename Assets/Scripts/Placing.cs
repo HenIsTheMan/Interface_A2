0ad54b8b13d3265877391c20c6e28d7dffbc4242
Henry <193541T@mymail.nyp.edu.sg>
2021-01-29 00:59:35 +0800

    Placing stuff

---
+++ Assets/Scripts/Placing.cs
@@ -0,0 +1,50 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace InterfaceA2 {
+    internal sealed class Placing: MonoBehaviour {
+        #region Fields
+
+        [SerializeField] private GameObject placingTextPrefab;
+        [SerializeField] private GameObject[] grayBars;
+        [SerializeField] private string[] texts;
+        int arrLen;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        public Placing() {
+            placingTextPrefab = null;
+            grayBars = null;
+            texts = null;
+            arrLen = 0;
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void Awake() {
+            UnityEngine.Assertions.Assert.IsNotNull(placingTextPrefab);
+            UnityEngine.Assertions.Assert.IsNotNull(grayBars);
+            arrLen = grayBars.Length;
+            UnityEngine.Assertions.Assert.AreEqual(arrLen, texts.Length);
+        }
+
+        private void Start() {
+            for(int i = 0; i < arrLen; ++i) {
+                Vector3 pos = ((RectTransform)grayBars[i].transform).localPosition;
+                GameObject placingTextGO = Instantiate(placingTextPrefab, new Vector3(pos.x + 15.0f, pos.y, 0), Quaternion.identity);
+                placingTextGO.transform.SetParent(gameObject.transform, false);
+                placingTextGO.name = "PlacingText" + i;
+                placingTextGO.GetComponent<TextMeshProUGUI>().text = texts[i];
+            }
+        }
+
+        #endregion
+    }
+}
