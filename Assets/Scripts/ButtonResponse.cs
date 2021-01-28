9f386cc1b418d29370fdc6c86a3b5eb89768cb08
hanyemi09 <54048632+hanyemi09@users.noreply.github.com>
2021-01-29 05:36:24 +0800

    Settings, Level , Garage

---
+++ Assets/Scripts/ButtonResponse.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonResponse : MonoBehaviour
+{
+
+    public Button[] ButtonList;
+    public GameObject[] GameObjectList;
+    public int selectedCharacter = 0;
+    private Button yourButton;
+    private bool isCanvasActive;
+
+    void Start()
+    {
+        GameObjectList[0].SetActive(false);
+        GameObjectList[1].SetActive(false);
+    }
+
+    public void SetActiveCar(string buttonName)
+    {
+        Debug.Log(buttonName);
+        if(buttonName == "CarOne")
+        {
+            GameObjectList[0].SetActive(true);
+            GameObjectList[1].SetActive(false);
+        }
+        else
+        {
+            GameObjectList[0].SetActive(false);
+            GameObjectList[1].SetActive(true);
+        }
+    }
+
+    public void Testing1234(string buttonName)
+    {
+        Debug.Log(buttonName);
+        if(isCanvasActive)
+        {
+            if (buttonName == "CarOne")
+            {
+                GameObjectList[0].SetActive(true);
+                GameObjectList[1].SetActive(false);
+            }
+            else
+            {
+                GameObjectList[0].SetActive(false);
+                GameObjectList[1].SetActive(true);
+            }
+        }
+    }
+
+    public void SetCanvasActive(bool canvasActive)
+    {
+        isCanvasActive = canvasActive;
+    }
+
+}
+
---
+++ Assets/Scripts/CarSelection.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class CarSelection : MonoBehaviour
+{
+    public GameObject[] GameObjectsList;
+    private int shownGameObjectIndex = -1;
+
+    private void Start()
+    {
+        for(int i = 0 ; i < GameObjectsList.Length; ++i )
+        {
+            GameObjectsList[i].SetActive(false);
+        }
+        SelectNextGameObject();
+    }
+    // Add this function as callback for your button's onClick event
+    public void SelectNextGameObject()
+    {
+        int index = shownGameObjectIndex >= GameObjectsList.Length - 1 ? -1 : shownGameObjectIndex;
+        SelectGameObject(index + 1);
+    }
+    public void SelectPreviousGameObject()
+    {
+        int index = shownGameObjectIndex <= 0 ? GameObjectsList.Length : shownGameObjectIndex;
+        SelectGameObject(index - 1);
+    }
+    public void SelectGameObject(int index)
+    {
+        if (shownGameObjectIndex >= 0)
+            GameObjectsList[shownGameObjectIndex].SetActive(false);
+        shownGameObjectIndex = index;
+        GameObjectsList[shownGameObjectIndex].SetActive(true);
+    }
+}
