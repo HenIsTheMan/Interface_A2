69143a4f6e565792d392dd0f398085ba81440f22
Henry <193541T@mymail.nyp.edu.sg>
2021-01-29 17:45:44 +0800

    MissileTravel.cs

---
+++ Assets/Scripts/MissileTravel.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace InterfaceA2 {
+    internal sealed class MissileTravel: MonoBehaviour {
+        #region Fields
+
+        [SerializeField] private float spd;
+        [SerializeField] private float yPosMax;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        private MissileTravel() {
+            spd = 0.0f;
+            yPosMax = 0.0f;
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void Start() {
+        }
+
+        private void FixedUpdate() {
+            if(((RectTransform)gameObject.transform).localPosition.y > yPosMax) {
+                Destroy(gameObject);
+                return;
+            }
+
+            transform.Translate(spd * Vector3.up * Time.deltaTime, Space.World);
+        }
+
+        #endregion
+    }
+}
