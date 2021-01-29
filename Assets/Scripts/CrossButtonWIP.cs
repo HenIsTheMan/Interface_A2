4cc3f302752578835bff662b478d12f962840521
Henry <193541T@mymail.nyp.edu.sg>
2021-01-29 21:41:41 +0800

    Pause button

---
+++ Assets/Scripts/CrossButtonWIP.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace InterfaceA2 {
+    internal sealed class CrossButtonWIP: MonoBehaviour {
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        private CrossButtonWIP() {
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+        #endregion
+
+        public void OnButtonClick() {
+            gameObject.transform.parent.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/PauseButtonScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace InterfaceA2 {
+    internal sealed class PauseButtonScript: MonoBehaviour {
+        #region Fields
+
+        [SerializeField] private GameObject WIP;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        public PauseButtonScript() {
+            WIP = null;
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void Awake() {
+            UnityEngine.Assertions.Assert.IsNotNull(WIP);
+        }
+
+        public void OnButtonClick() {
+            WIP.SetActive(true);
+        }
+
+        #endregion
+    }
+}
