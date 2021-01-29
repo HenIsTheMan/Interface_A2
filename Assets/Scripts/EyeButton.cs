91f4574e01af56256c1dceaa2eaaa688c1f375cc
Henry <193541T@mymail.nyp.edu.sg>
2021-01-29 21:54:59 +0800

    Driver

--- Assets/Scripts/EyeButton.cs
+++ Assets/Scripts/EyeButton.cs
@@ -3,6 +3,9 @@
 namespace InterfaceA2 {
     internal sealed class EyeButton: MonoBehaviour {
         #region Fields
+
+        [SerializeField] private GameObject driverGO;
+
         #endregion
 
         #region Properties
@@ -11,12 +14,21 @@ namespace InterfaceA2 {
         #region Ctors and Dtor
 
         public EyeButton() {
-
+            driverGO = null;
         }
 
         #endregion
 
         #region Unity User Callback Event Funcs
+
+        private void Awake() {
+            UnityEngine.Assertions.Assert.IsNotNull(driverGO);
+        }
+
+        public void OnButtonClick() {
+            driverGO.SetActive(!driverGO.activeSelf);
+        }
+
         #endregion
     }
 }
